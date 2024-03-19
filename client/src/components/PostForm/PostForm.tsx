1bf2670b6581b5c0378bd0ab2799a001c1de59c8
Sergey Tyurikov <project-present@yandex.ru>
2024-03-19 18:36:37 +0700

    feat: валидация формы составления поста, использование библиотеки react-hook-form

--- client/src/components/PostForm/PostForm.tsx
+++ client/src/components/PostForm/PostForm.tsx
@@ -1,36 +1,50 @@
-import {FC, FormEventHandler, useState} from 'react';
+import {FC} from 'react';
+import {useMutation} from "@tanstack/react-query";
+import { z } from 'zod';
+import { useForm } from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
 
 import { Button } from '../Button';
 import { FormField } from '../FormField';
 import './PostForm.css';
-import {useMutation} from "@tanstack/react-query";
 import {createPost} from "../../api/Post.ts";
 import {queryClient} from "../../api/queryClient.ts";
 
 export interface IPostFormProps {}
 
+const CreatePostSchema = z.object({
+    text: z.string().min(10, "Длина поста должна быть не менее 10 символов"),
+})
+
+type CreatePostForm = z.infer<typeof CreatePostSchema>
+
 export const PostForm: FC<IPostFormProps> = () => {
-    const [text, setText] = useState("")
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<CreatePostForm>({
+        resolver: zodResolver(CreatePostSchema),
+    })
 
     const createPostMutation = useMutation({
-        mutationFn: () => createPost(text),
+        mutationFn: createPost,
         onSuccess() {
             queryClient.invalidateQueries({queryKey: ["posts"]})
         }
     }, queryClient)
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-      event.preventDefault()
-      createPostMutation.mutate()
-  };
+
 
   return (
-    <form className="post-form" onSubmit={handleSubmit}>
-      <FormField label="Текст поста">
+    <form className="post-form" onSubmit={handleSubmit(({ text }) => {
+        createPostMutation.mutate(text)
+    })}>
+      <FormField label="Текст поста" errorMessage={errors.text?.message}>
         <textarea
             className="post-form__input"
-            value={text}
-            onChange={(event) => setText(event.currentTarget.value)}
+            {...register("text")}
         />
       </FormField>
 
@@ -38,3 +52,15 @@ export const PostForm: FC<IPostFormProps> = () => {
     </form>
   );
 };
+
+
+/*  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+      event.preventDefault()
+
+      if(text.length > 10) {
+          createPostMutation.mutate()
+      } else {
+          setErrorMessage("Введите более 10 символов")
+      }
+
+  };*/
